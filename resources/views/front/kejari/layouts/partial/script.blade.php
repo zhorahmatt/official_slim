c7e0cd4d48d6663357676c75d598937115a233c3
zhorahmatt <info.rahmathidayat@gmail.com>
2018-01-24 12:14:19 +0800

    update route

--- resources/views/front/kejari/layouts/partial/script.blade.php
+++
@@ -1,6 +0,0 @@
-<script src="{{ asset('assets') }}/front/kejari/js/kejari.js"></script>
-<script>
-	@if ( Session::has('subscribe_success') )
-		$('#subscribeSuccessModal').modal('show');
-	@endif
-</script>
